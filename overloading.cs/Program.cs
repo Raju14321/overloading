342baa943ab353adde3a0ecb489aabb9a2edb3e9
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-03-01 09:42:54 +0530

    Add files via upload

---
+++ overloading.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace overloading.cs
+{
+    class Program
+    {
+        public void Test()
+        {
+            Console.WriteLine("metho 1");
+        }
+        public void Test(int i)
+        {
+            Console.WriteLine("Gives num 1432");
+        }
+        public void Test(string a)
+        {
+            Console.WriteLine("raju");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            p.Test(10);
+            p.Test("lucky");
+            Console.ReadLine();
+        }
+    }
+}
